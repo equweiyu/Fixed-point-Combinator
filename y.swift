f28521431d6847f9734affdb87fdf08c65c56296
baitianwei <baitianwei@kezaihui.com>
2019-05-07 18:09:19 +0800

    swift y

--- y.swift
+++ y.swift
@@ -1,12 +1,6 @@
-private struct Mu<T> {
-    let mu:(Mu) -> (T) -> T
+func Y<T>(f:@escaping (@escaping (T) -> T) -> ((T) -> T)) -> ((T) -> T) {
+    return {(x:T) -> T in f(Y(f: f))(x)}
 }
-
-func Y<T>(r:@escaping (@escaping (T) -> T) -> ((T) -> T)) -> ((T) -> T) {
-    let h = Mu(mu: {(f:Mu) in r({(x) in f.mu(f)(x)})})
-    return h.mu(h)
-}
-
 func main() {
     let fib = Y { g in {$0 <= 2 ? 1 : g($0-1) + g($0-2)} }
     print(fib(10))
